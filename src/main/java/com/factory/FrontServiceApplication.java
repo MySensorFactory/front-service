93316aa313542cbaba74f532dd352543439774fa
Damian Wójcik <damian.a.wojcik@comarch.pl>
2023-09-24 15:49:37 +0200

    Unused redundant annotations

--- src/main/java/com/factory/FrontServiceApplication.java
+++ src/main/java/com/factory/FrontServiceApplication.java
@@ -2,12 +2,8 @@ package com.factory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
-@EnableWebFlux
-@EnableFeignClients
 public class FrontServiceApplication {
 
     public static void main(String[] args) {
